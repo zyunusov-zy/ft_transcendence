2f5a72821f67903005d9e6658b0ebedda5601a43
doduwole <doduwole@student.42wolfsburg.de>
2024-09-09 18:26:19 +0200

    docker working but the env are buggy

--- pong/create_db_and_superuser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create the superuser and grant permissions
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  ALTER ROLE $POSTGRES_USER WITH SUPERUSER;
-EOSQL
