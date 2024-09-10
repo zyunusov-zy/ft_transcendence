bbf5f5b85f853d7aed40b0fd96566f4ebde1c334
doduwole <doduwole@student.42wolfsburg.de>
2024-09-10 10:23:25 +0200

    removed some prints in settings.py

--- pong/pong/settings.py
+++ pong/pong/settings.py
@@ -6,12 +6,9 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(BASE_DIR)
 
 load_dotenv()
 
-for key in os.environ:
-    print(f"{key}: {os.getenv(key)}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -98,9 +95,6 @@ DATABASES = {
     }
 }
 
-print(os.environ.get('DATABASE_PASSWORD'))
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -133,8 +127,6 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-print(MEDIA_ROOT)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
